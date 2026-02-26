ecf219ac18d84339df116b65c8fac23bff6eb731
vgungor <mvolkang@gmail.com>
2026-02-26 15:42:35 +0300

    Ders kodları ve sunum eklemesi

---
+++ h2_sunum_kodlar/2-ilk_kod.js
@@ -0,0 +1,12 @@
+var i = 10;
+var j = 5;
+
+var carpma = i * j;
+var toplama = i + j;
+var bolme = i / j;
+var kalan = i % j;
+
+console.log("carpma: " + carpma);
+console.log("toplama: " + toplama);
+console.log("bolme: " + bolme);
+console.log("kalan: " + kalan);
